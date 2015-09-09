a258f8aad7b6927ec6bbf00bdb9cef295c1b2b6b
Miinghan Chen <minghan2@andrew.cmu.edu>
2015-09-08 21:31:09 -0700

    first commit

---
+++ app/src/main/java/ExceptionHelper/CreateAccountException.java
@@ -0,0 +1,11 @@
+package ExceptionHelper;
+
+/**
+ * Created by chosen12 on 15/4/9.
+ */
+public class CreateAccountException extends Exception {
+    public CreateAccountException(String msg) {
+        super(msg);
+    }
+
+}
